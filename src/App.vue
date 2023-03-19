b53714b8e7b92ac78c4cabdbf886b8662d04a506
Henry <henrykc24@gmail.com>
2023-03-19 22:55:33 +0100

    Add created lifecycle

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,88 @@
 <template>
-  <h1>Hello World!</h1>
+  <div class="container">
+    <Header  title="Task tracker" />
+  </div>
 </template>
 
 <script>
-export default {
-  name: "App",
-  components: {},
-};
+  import Header from './components/Header';
+  export default {
+    name: "App",
+    components: {
+      Header,
+    },
+    data() {
+      return {
+        tasks: []
+      }
+    },
+    created() {
+      this.tasks = [
+        {
+          id: 1,
+          text: 'Doctors Appointments',
+          day: 'March 1 at 2:30pm',
+          reminder: true,
+        },
+        {
+          id: 2,
+          text: 'Meeting at School',
+          day: 'March 3 at 1:30pm',
+          reminder: true,
+        },
+        {
+          id: 3,
+          text: 'Food Shopping',
+          day: 'March 3 at 11:00am',
+          reminder: false,
+        }
+      ]
+    }
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: "Poppins", sans-serif;
+  }
+
+  .container {
+    max-width: 500px;
+    margin: 30px auto;
+    overflow: auto;
+    min-height: 300px;
+    border: 1px solid steelblue;
+    padding: 30px;
+    border-radius: 5px;
+  }
+
+  .btn {
+    display: inline-block;
+    background: #000;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    margin: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    font-size: 15px;
+    font-family: inherit;
+  }
+
+  .btn:focus {
+    outline: none;
+  }
+
+  .btn-block {
+    display: block;
+    width: 100%;
+  }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="onClick()" :style="{backgroundColor: color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+  export default {
+    name: "Button",
+    props: {
+      text: {
+        type: String,
+        default: 'Add',
+      },
+      color: {
+        type: String,
+        default: 'black',
+      }
+    },
+    methods: {
+      onClick() {
+        console.log('You clicked on the button');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
